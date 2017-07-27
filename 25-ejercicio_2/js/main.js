8b6383894d975d1b2fcf90d41e1cec3deec16ad5
InaxioA <30486253+InaxioA@users.noreply.github.com>
2017-07-27 05:22:01 +0200

    Update main.js
    El 'this' que tenias en la linea 8, hacia referencia a un elemento de tipo '.panel_cabecera', no '.panel_contenido' y no ignora el pinchado. Con lo cual, si intentas cerrar el que está abierto, no lo conseguirás, dado que primero lo cierras con el slideUp() y luego vuelves a abrir con el slideToggle().
    Dejo mi propuesta de código en la línea 8.

--- 25-ejercicio_2/js/main.js
+++ 25-ejercicio_2/js/main.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
     // Event listeners
     $('.panel__cabecera').on('click', function (evt) {
         evt.preventDefault();
-        $('.panel__contenido').not(this).each(function () {
-           $(this).slideUp();
-        });
+        $('.panel__cabecera').not(this).siblings('.panel__contenido').slideUp(); // cerrar el resto
         $(this).siblings('.panel__contenido').slideToggle();
     });
 });
